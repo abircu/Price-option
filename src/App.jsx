260fa876523c5a350f152ef7079d890f06af94b3
abircu <mdabircse4873@gmail.com>
2023-09-18 21:03:10 +0530

    crating price-options website and complete installation proces

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl text-center font-semibold text-sky-600">
+          hello react tailwind
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
